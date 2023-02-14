5aeeb291750571fc11f893bebf0d5d6af8311162
Craig Pettifor <craig_petti4@hotmail.com>
2023-02-14 09:58:05 +0100

    resize button work

--- scripts.js
+++ scripts.js
@@ -1,26 +1,28 @@
 const board = document.querySelector("#board");
-let size = 16;
+drawBoard();
 
-for (let i = 0; i < size; i ++) {
-    const row1 = document.createElement("row1");
-    row1.classList.add("row1");
-    board.appendChild(row1);
+function drawBoard(size = 16) {
 
-    for (let j = 0; j < size; j++) {
-        const pixel = document.createElement("pixel");
-        pixel.classList.add("pixel");
-        pixel.textContent = "__";
-        pixel.style.color = "grey";
-        pixel.style.backgroundColor = "grey";
-        pixel.addEventListener("mouseover", function(){
-            console.log("mouseover triggered at " + i + " " + j)
-            pixel.style.backgroundColor = "black";
-            pixel.style.color = "black";
-        });
-        row1.appendChild(pixel);
+    for (let i = 0; i < size; i ++) {
+        const row1 = document.createElement("row1");
+        row1.classList.add("row1");
+        board.appendChild(row1);
+
+        for (let j = 0; j < size; j++) {
+            const pixel = document.createElement("pixel");
+            pixel.classList.add("pixel");
+            pixel.textContent = "__";
+            pixel.style.color = "grey";
+            pixel.style.backgroundColor = "grey";
+            pixel.addEventListener("mouseover", function(){
+                console.log("mouseover triggered at " + i + " " + j)
+                pixel.style.backgroundColor = "black";
+                pixel.style.color = "black";
+            });
+            row1.appendChild(pixel);
+        }
     }
 }
-
 const buttons = document.querySelectorAll("button")
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -32,6 +34,15 @@ buttons.forEach((button) => {
             });
         }
         else {
+            size = prompt("Set size");
+            document.querySelector("#board").innerHTML = "";
+            if (size > 100) {
+                alert("Size too large. Generating 16x16 board.")
+                drawBoard();
+            }
+            else {
+                drawBoard(size);
+            }
             
         }
     })
